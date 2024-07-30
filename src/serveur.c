8a84779588a95f04bb28c1b2c043b087ba8d467f
mophe94 <dylan-bajeux@hotmail.com>
2024-07-30 17:27:03 +0200

    start implementation for serveur.c

--- src/serveur.c
+++ src/serveur.c
@@ -3,14 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   serveur.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dbajeux <dbajeux@student.s19.be>           +#+  +:+       +#+        */
+/*   By: dbajeux <dbajeux@student.19.be>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/30 17:09:56 by dbajeux           #+#    #+#             */
-/*   Updated: 2024/07/30 18:36:33 by dbajeux          ###   ########.fr       */
+/*   Updated: 2024/07/30 17:25:05 by dbajeux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(void)
+# include "../inc/minitalk.h"
+
+#include <signal.h>
+#include <unistd.h>
+
+void handler(int bit)
 {
     
+}
+
+int main(void)
+{
+    ft_printf(" l'id processus de mon serveur est : %d",getpid());
+    signal(SIGUSR1, handler);
+    signal(SIGUSR2,handler);
+    while (1)
+        pause();
+    return (0);
 }
